cf805ce54db5fe945f410d4d1c87daac2cb4987a
Anastasia <nastya70sv@mail.ru>
2022-11-21 11:48:13 +0300

    Added HW 1 and HW 2

---
+++ HW_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число N : ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+string NumText = Convert.ToString(Number);
+if (NumText.Length < 6)
+{
+    if (NumText[1] == NumText[5] && NumText[2] == NumText[4])
+    {
+      Console.WriteLine(" Это палиндром ");
+    }
+    else 
+    {
+    Console.WriteLine(" Число не является палиндром ");
+    }
+}
+else
+{
+    Console.WriteLine(" Вы ввели не пятизначное число ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     if (NumText[1] == NumText[5])  
+//     {
+//         if (NumText[2] == NumText[4])
+//         {
+//             Console.WriteLine(" Это палиндром ");
+//         }
+
+//     else
+//     {
+//         Console.WriteLine(" Число не является палиндром ");
+//     }
+// }
+// else
+
+//     Console.WriteLine(" Вы ввели не пятизначное число ");
---
+++ HW_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координату первой точки Xa: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки Ya: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки Za: ");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату второй точки Xb: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки Yb: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки Zb: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+ double distance = Math.Round(Math.Sqrt((Math.Pow(xb - xa ,2))+(Math.Pow(yb - ya,2)) + (Math.Pow(zb - za,2))), 2);
+ Console.WriteLine($"растояние между точками {distance} ");
