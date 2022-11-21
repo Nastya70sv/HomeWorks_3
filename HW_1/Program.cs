9dca07cf28858abc998138d46a0e8e1f7ef93ece
Anastasia <nastya70sv@mail.ru>
2022-11-21 11:57:41 +0300

    Added HW 1 and HW 2 and HW 3

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -6,7 +6,7 @@ int Number = Convert.ToInt32(Console.ReadLine());
 string NumText = Convert.ToString(Number);
 if (NumText.Length < 6)
 {
-    if (NumText[1] == NumText[5] && NumText[2] == NumText[4])
+    if (NumText[0] == NumText[4] && NumText[1] == NumText[3])
     {
       Console.WriteLine(" Это палиндром ");
     }
